f6abe287bdd8d7791be1fac7763f86e6afa431b1
muhammadahad96 <muhammadahad9615@gmail.com>
2020-09-06 22:37:17 +0500

    Updates

---
+++ precache-manifest.4357a9b5af2821461e6d16ee72ab3480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d1fbb5ea2352b1e60578758936e6d1",
+    "url": "/react-hooks-todoapp/index.html"
+  },
+  {
+    "revision": "ab4d91cc16cc06d4a089",
+    "url": "/react-hooks-todoapp/static/css/main.37e88269.chunk.css"
+  },
+  {
+    "revision": "858742a52fd19c184479",
+    "url": "/react-hooks-todoapp/static/js/2.d6e2d1b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-todoapp/static/js/2.d6e2d1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4d91cc16cc06d4a089",
+    "url": "/react-hooks-todoapp/static/js/main.4950aa43.chunk.js"
+  },
+  {
+    "revision": "02f077053522d5502e15",
+    "url": "/react-hooks-todoapp/static/js/runtime-main.5fc2f264.js"
+  }
+]);
